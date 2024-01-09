af48a8cb3fbea65342ba8263b67b2db4c8f192e3
Luan <42867942+luansapelli@users.noreply.github.com>
2024-01-08 22:02:41 -0300

    add framebuffer callback and changing the window background color

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include "iostream"
+#include <glad/glad.h>
 #include "GLFW/glfw3.h"
 
 const char* window_title = "Learn OpenGL";
 constexpr int window_width = 1280;
 constexpr int window_height = 800;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    // Make sure the viewport matches the new window dimensions
+    glViewport(0, 0, width, height);
+}
 
 int main() {
     std::cout << "Starting GLFW..." << std::endl;
@@ -14,6 +20,16 @@ int main() {
         std::cout << "glfwInit -> Failed to initialize GLFW" << std::endl;
         return -1;
     }
+
+    // Set the OpenGL version to 4.6 and use the core profile
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    // Forward compatibility with macOS
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
     // Create a windowed mode window and its OpenGL context
     GLFWwindow* window = glfwCreateWindow(window_width, window_height, window_title, nullptr, nullptr);
@@ -27,19 +43,25 @@ int main() {
     // Make the window's context current
     glfwMakeContextCurrent(window);
 
-    // Set the OpenGL version to 4.6 and use the core profile
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // Load GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
 
-#ifdef __APPLE__
-    // Forward compatibility with macOS
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
+    // Set the viewport to match the window's size
+    glViewport(0, 0, window_width, window_height);
+
+    // Register the framebuffer_size_callback function on window resize
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // Loop until the user closes the window
     while (!glfwWindowShouldClose(window)) {
-        // @TODO -> Render stuff here
+        // Render stuff here
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         // Swap front and back buffers
         glfwSwapBuffers(window);
